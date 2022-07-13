7b3fa22116f19da694409524b49928858b318632
helderzack <helderzacarias@outlook.com>
2022-07-12 22:03:18 -0300

    I've modified the package.json to be able to call the backend application, which is running on localhost. After that, the fetch was implemented on the useEffect hook, and the data is correctly beign fetched.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { TransactionWrapper } from './entities/TransactionWrapper';
 
 function App() {
+  const [transactionsWrapper, setTransactionWrappers] = useState<TransactionWrapper[]>([]);
+
+  useEffect(() => {
+    fetch('/api/v1/fetchTrades')
+      .then((response) => response.json()).then(data => setTransactionWrappers(data));
+  }, []);
+
   return (
     <div>
       <header>
-       <h1>NBA Trades Digger</h1>
+        <h1>NBA Trades Digger</h1>
+        <main>
+          {transactionsWrapper.map((transactionWrapper: TransactionWrapper, index) =>
+            <div key={index}>
+              {transactionWrapper.transactions.map((transaction, index) =>
+                <h2>{`${index + 1} ${transaction.description}`}</h2>)}
+            </div>)};
+        </main>
       </header>
     </div>
   );
